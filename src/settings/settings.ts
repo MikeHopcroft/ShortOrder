5d18aa4510604c6b2e6cdfe0744fdb6e5b8bc074
Arman Rahman <arrahm@microsoft.com>
2018-10-17 20:35:10 -0400

    removed env file calls

--- src/settings/settings.ts
+++ src/settings/settings.ts
@@ -2,15 +2,15 @@ import * as dotenv from 'dotenv';
 
 dotenv.config();
 
-export const YAML_MENU_FILE = checkEnv('YAML_MENU_FILE');
-export const YAML_TEST_FILE = checkEnv('YAML_TEST_FILE');
+// export const YAML_MENU_FILE = checkEnv('YAML_MENU_FILE');
+// export const YAML_TEST_FILE = checkEnv('YAML_TEST_FILE');
 
-function checkEnv(key:string) {
-    const value = process.env[key];
-    if (value) {
-        return value;
-    }
-    else {
-        throw new Error(`Environment variable ${key} undefined.`);
-    }
-}
+// function checkEnv(key:string) {
+//     const value = process.env[key];
+//     if (value) {
+//         return value;
+//     }
+//     else {
+//         throw new Error(`Environment variable ${key} undefined.`);
+//     }
+// }
