c2560050a1a0af870cd23d3a1a7f1fcdbc46c174
Michael Hopcroft <mhop@microsoft.com>
2019-03-27 12:24:48 -0700

    test_runner.ts

---
+++ samples/test_runner.ts
@@ -0,0 +1,44 @@
+import * as fs from 'fs';
+import * as minimist from 'minimist';
+import * as path from 'path';
+
+import {
+    AnyToken,
+    setup,
+    TestSuite,
+    TokenizerFunction
+} from '../src';
+
+function go() {
+    const args = minimist(process.argv.slice(2));
+
+    const defaultTestFile = './data/restaurant-en/test_suite.yaml';
+    const testFile = path.resolve(__dirname, args['f'] || defaultTestFile);
+
+    const showAll = (args['a'] === true);
+    const suiteFilter = (args['s']);
+
+    if (suiteFilter) {
+        console.log(`Running tests in suite: ${suiteFilter}`);
+    }
+    else {
+        console.log('Running all tests.');
+    }
+
+    const world = setup(
+        path.join(__dirname, './data/restaurant-en/menu.yaml'),
+        path.join(__dirname, './data/restaurant-en/intents.yaml'),
+        path.join(__dirname, './data/restaurant-en/attributes.yaml'),
+        path.join(__dirname, './data/restaurant-en/quantifiers.yaml'),
+        false
+    );
+
+    // Set up tokenizer
+    const tokenizer: TokenizerFunction = (utterance: string): IterableIterator<AnyToken> =>
+        (world.unified.processOneQuery(utterance) as AnyToken[]).values();
+
+    const suite = TestSuite.fromYamlString(fs.readFileSync(testFile, 'utf8'));
+    suite.run(world, showAll, suiteFilter, tokenizer);
+}
+
+go();
